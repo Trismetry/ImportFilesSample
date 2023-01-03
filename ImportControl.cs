bc034e2c0ed28b0eae58e04cc4171c5148c8d0fd
Trismetry <85286081+Trismetry@users.noreply.github.com>
2023-01-03 14:21:28 +0200

    Add files via upload

---
+++ ImportControl.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+
+public enum ImportEntity
+{
+    Associations = 0,
+    Units = 1,
+    Accounts = 2,
+    AccountContacts = 3,
+    Notes = 4,
+    FirmWOTypes = 5,
+    FirmVIOTypes = 6,
+    AssociationVIOTypes = 7,
+    AssociationVIOSubTypes = 8,
+    AssociationVIOLocations = 9,
+    AssociationWOTypes = 10,
+    AssociationWOSubTypes = 11,
+    AssociationWOLocations = 12,
+    Violations = 13,
+    WorkOrders = 14,
+    ARCPlans = 15,
+    TrackedItems = 16,
+    Pets = 17,
+    FactResponses = 18,
+    Vehicles = 19,
+    Visitors = 20,
+    Transactions = 21,
+    Vendors = 22,
+    VendorAssociations = 23,
+    FirmARCTypes = 24,
+    AssociationARCTypes = 25,
+    BankValidationXrefs = 26,
+    Additional1099Amounts = 27,
+    AssociationTransTypes = 28,
+    FirmTransTypes = 29,
+    BankAccounts = 30
+}
+public struct DatabaseColumn
+{
+    public string ColumnName;
+    public Type ColumnType;
+    public object DefaultValue;
+
+    public DatabaseColumn(string DBColumnName, Type ColumnType, object DefaultValue = null)
+    {
+        this.ColumnName = DBColumnName;
+        this.ColumnType = ColumnType;
+        this.DefaultValue = DefaultValue;
+    }
+}
+
+/// <summary>
+/// Base class to user controls on ImportData page
+/// </summary>
+public abstract class ImportControl : UserControl
+{
+    public struct ImportColumn
+    {
+        public string ColumnName;
+        public bool Required;
+        public bool CustomHandler;
+        public Type ColumnType;
+        public int? MaxLength;
+        public string[] AllowedValues;
+        public int? MinValue;
+        public int? MaxValue;
+        public bool RequireOneOfSelectedColumns;
+    }
+
+    protected ImportColumn[][] ImportColumnsInsert = new ImportColumn[Enum.GetValues(typeof(ImportEntity)).Length][];
+    protected ImportColumn[][] ImportColumnsUpdate = new ImportColumn[Enum.GetValues(typeof(ImportEntity)).Length][];
+
+
+    public ImportEntity Entity
+    {
+        get
+        {
+            if (Session["ImportEntity"] != null)
+                return (ImportEntity)Session["ImportEntity"];
+            else
+                throw new ApplicationException("ImportEntity was not set");
+        }
+        set
+        {
+            Session["ImportEntity"] = value;
+        }
+    }
+    public bool UpdateMode
+    {
+        get
+        {
+            if (Session["UpdateMode"] != null)
+                return (bool)Session["UpdateMode"];
+            else
+                throw new ApplicationException("UpdateMode was not set");
+        }
+        set
+        {
+            Session["UpdateMode"] = value;
+        }
+    }
+    public DataTable UserData
+    {
+        get
+        {
+            if (Session["UserData"] != null)
+                return (DataTable)Session["UserData"];
+            else
+                return null;
+        }
+        set
+        {
+            Session["UserData"] = value;
+        }
+    }
+    protected DataTable ImportData
+    {
+        get
+        {
+            if (Session["ImportData"] != null)
+                return (DataTable)Session["ImportData"];
+            else
+                return null;
+        }
+        set
+        {
+            Session["ImportData"] = value;
+        }
+    }
+
+    public string ErrorMessage
+    {
+        get;
+        protected set;
+    }
+
+    public abstract bool MoveNext();
+    public abstract bool Display();
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+
+        if (!IsPostBack)
+        {
+            Session["ImportEntity"] = null;
+            Session["UpdateMode"] = null;
+            UserData = null;
+        }
+    }
+}
---
+++ ImportConverterBase.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+
+/// <summary>
+/// Finds value for any of SaveTable's column using column values in UserTable
+/// </summary>
+public class ImportConverterBase
+{
+   
+    public DateTime? ParseUserDateTime(string UserValue)
+    {
+        IFormatProvider format = new CultureInfo("en-US");
+
+        if (DateTime.TryParse(s: UserValue, result: out DateTime date, styles: DateTimeStyles.None, provider: format))
+            return date;
+        else return null;
+
+    }
+    public TimeSpan? ParseUserTime(string UserValue)
+    {
+        if (TimeSpan.TryParse(s: UserValue, result: out TimeSpan time))
+            return time;
+        else return null;
+
+    }
+    public bool ParseUserBoolean(string UserValue)
+    {
+        bool Result = false;
+
+        if (!string.IsNullOrEmpty(UserValue) && (UserValue.ToUpper().Equals("1", StringComparison.OrdinalIgnoreCase) ||
+                UserValue.ToUpper().Equals("Y", StringComparison.OrdinalIgnoreCase)))
+            Result = true;
+
+        return Result;
+    }
+    public static ImportConverterBase Create(ImportEntity Entity, bool UpdateMode)
+    {
+        ImportConverterBase Converter;
+
+        switch (Entity)
+        {
+            case ImportEntity.Accounts:
+                Converter = new AccountImportConverter();
+                break;
+            case ImportEntity.AccountContacts:
+                Converter = new AccountMailingAddressImportConverter();
+                break;
+            case ImportEntity.Associations:
+                Converter = new AssociationImportConverter();
+                break;
+            case ImportEntity.WorkOrders:
+                Converter = new WorkOrderImportConverter();
+                break;
+            case ImportEntity.Violations:
+                Converter = new ViolationImportConverter();
+                break;
+            case ImportEntity.ARCPlans:
+                Converter = new ARCPlanImportConverter();
+                break;
+            case ImportEntity.AssociationVIOSubTypes:
+                Converter = new AssociationVIOSubTypeImportConverter();
+                break;
+            case ImportEntity.AssociationWOSubTypes:
+                Converter = new AssociationWOSubTypeImportConverter();
+                break;
+            case ImportEntity.Vendors:
+                Converter = new VendorImportConverter();
+                break;
+            case ImportEntity.FirmTransTypes:
+                if (!UpdateMode)
+                    Converter = new FirmTransTypeImportConverter();
+                else
+                    Converter = new FirmTransTypeUpdateImportConverter();
+                break;
+            case ImportEntity.BankAccounts:
+                Converter = new BankAccountImportConverter();
+                break;
+            default:
+                Converter = new ImportConverterBase();
+                break;
+        }
+
+        return Converter;
+    }
+    public virtual object Convert(DataRow UserTableRow, DataColumn DatabaseColumn, bool isUpdateMode)
+    {
+        object Result = null;
+        try
+        {
+
+            if (UserTableRow.Table.Columns.Contains(DatabaseColumn.ColumnName)
+                )
+            {
+                string UserValue = UserTableRow[DatabaseColumn.ColumnName].ToString();
+
+                if (UserValue.ToLower() == "null" && DatabaseColumn.AllowDBNull)
+                {
+                    return DatabaseColumn.DefaultValue; //  change string value of "null" to CLR null value
+                }
+
+                if (DatabaseColumn.DataType == typeof(DateTime))
+                {
+                    DateTime? dt = ParseUserDateTime(UserValue);
+                    if (dt != null) Result = (DateTime)dt;
+                }
+                else if (DatabaseColumn.DataType == typeof(TimeSpan))
+                {
+                    TimeSpan? dt = ParseUserTime(UserValue);
+                    if (dt != null) Result = (TimeSpan)dt;
+                }
+                else
+                {
+                    if (DatabaseColumn.DataType == typeof(bool))
+                    {
+                        if (string.IsNullOrEmpty(UserValue))
+                            Result = DatabaseColumn.DefaultValue;
+                        else
+                            Result = ParseUserBoolean(UserValue);
+                    }
+                    else
+                    {
+                        Result = System.Convert.ChangeType(UserValue, DatabaseColumn.DataType);
+                    }
+                }
+
+            }
+            else if (isUpdateMode && DatabaseColumn.DataType == typeof(bool))
+            {
+
+            }
+            else
+                Result = DatabaseColumn.DefaultValue;
+
+        }
+        catch (Exception ex) { }
+
+        return Result;
+    }
+}
